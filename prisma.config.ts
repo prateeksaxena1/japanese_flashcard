2b92cd06bbb0f5e15fa87dd5b6c6ca4a9866395b
Prateek Saxena <pksaxena6453@gmail.com>
2026-05-25 15:10:59 +0530

    Security: remove hardcoded database conncection fallback

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_wiu87LRFtSIg@ep-fancy-band-aqt1t5jk.c-8.us-east-1.aws.neon.tech/neondb?sslmode=verify-full",
+    url: process.env.DATABASE_URL,
   },
 });
